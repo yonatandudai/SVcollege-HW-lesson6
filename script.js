e43599b99f780c27dccaefc57c7c6f90f61bedb7
yonatandudai <duday@post.bgu.ac.il>
2024-12-13 19:26:48 +0200

    added Q5.

--- script.js
+++ script.js
@@ -60,13 +60,38 @@ function littleAndBig(str1, str2){
     }
     return counter;
 }
+
 //Q5.
+function bubbleSortString(str) {
+    let arr = str.split(''); // Convert the string to an array of characters
+    let n = arr.length;
+
+    // Bubble sort algorithm
+    for (let i = 0; i < arr.length; i++) {
+
+        // Last i elements are already in place  
+        for (let j = 0; j < (arr.length - i - 1); j++) {
+
+            // Checking if the item at present iteration 
+            // is greater than the next iteration
+            if (arr[j] > arr[j + 1]) {
+
+                // If the condition is true
+                // then swap them
+                let temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+    }
+
+    return arr.join(''); // Convert the sorted array back to a string
+}
 
 
 // main
 // console.log(idChecker());
-// isPrime(7);
-// console.log(findCapital("abCdEf"));
-//console.log(littleAndBig("hi","hihihi"));
-
-
+isPrime(7);
+console.log(findCapital("abCdEf"));
+console.log(littleAndBig("hi","hihihi"));
+console.log(bubbleSortString("fcab"));
